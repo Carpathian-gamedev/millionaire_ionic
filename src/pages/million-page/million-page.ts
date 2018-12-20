d1319d3108d78e99bc1a09b555d80c15ea14b95e
osc <scherbakoleg94@gmail.com>
2018-12-20 20:25:40 +0200

    grid bg and help btn color changes

--- src/pages/million-page/million-page.ts
+++ src/pages/million-page/million-page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import { SharedService } from '../../helpers/scripts/shared-service';
 
 @Component({
 	selector: 'million-page',
 	templateUrl: 'million-page.html'
 })
 export class MillionPage {
-	constructor(public navCtrl: NavController, public viewCtrl: ViewController, public params: NavParams) {
+	constructor(public navCtrl: NavController, public sharedService: SharedService, public viewCtrl: ViewController, public params: NavParams) {
 		this.setBackBtnAction();
 	}
 
